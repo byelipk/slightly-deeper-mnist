67871cf630dc6d21a195e592697310063cc9d020
byelipk <byelipk@gmail.com>
2017-03-24 09:17:46 -0400

    Add documentation for take_n

--- utils/take_n.py
+++ utils/take_n.py
@@ -2,13 +2,38 @@ from collections import Counter
 
 def take_n(data, n, test_condition=lambda x: True):
     """
-    Return the index position of the first N items that appear as keys
-    in a "keeplist" and which also match a condition.
+    Return index position of first N items that match a test condition.
+
+    Parameters
+    ==========
+
+    :data: An enumerable, such as a list.
+    :n: The number of times we can take an item.
+    :test_condition: Each item we keep must meet the condition.
+
+    Summary
+    =======
+
+    Sometimes you want to filter a list based on a condition
+    but you have an upper bound of how many times you can
+    collect an item from a list. This function helps you
+    with this problem.
+
+    For example, if you only wanted to process integers less
+    than three and could only sample each inter twice:
+
+        data = [1,2,1,4,5,2,2]
+        idx  = list(take_n(data, 2, lambda x: x < 3))
+        idx  == [0,1,2,5]
+
+        ints = data[idx]
+        ints == [1,2,1,2]
+
     """
     keeplist = Counter()
 
     for idx, item in enumerate(data):
-        
+
         # Use the condition to filter out unwanted items
         if test_condition(item):
 
