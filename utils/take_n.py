b0ae2f141228ba558ab197e96e484bbf7362f2aa
byelipk <byelipk@gmail.com>
2017-03-24 09:18:50 -0400

    Fix typo

--- utils/take_n.py
+++ utils/take_n.py
@@ -15,12 +15,12 @@ def take_n(data, n, test_condition=lambda x: True):
     =======
 
     Sometimes you want to filter a list based on a condition
-    but you have an upper bound of how many times you can
+    and you have an upper bound of how many times you can
     collect an item from a list. This function helps you
     with this problem.
 
     For example, if you only wanted to process integers less
-    than three and could only sample each inter twice:
+    than three and could only sample each integer twice:
 
         data = [1,2,1,4,5,2,2]
         idx  = list(take_n(data, 2, lambda x: x < 3))
